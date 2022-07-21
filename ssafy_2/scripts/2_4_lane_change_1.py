c70d10256477c7b4d99fd5491d050879185c1331
mdmdzero <mdmdzerozero@naver.com>
2022-07-21 14:45:42 +0900

    add lc_ex

--- ssafy_2/scripts/2_4_lane_change_1.py
+++ ssafy_2/scripts/2_4_lane_change_1.py
@@ -3,6 +3,7 @@
 
 import rospy
 import rospkg
+import tf
 from math import cos,sin,pi,sqrt,pow
 import numpy as np
 from geometry_msgs.msg import Point32,PoseStamped
@@ -15,6 +16,7 @@ class lc_path_pub :
         rospy.Subscriber("/Ego_topic", EgoVehicleStatus, self.statusCB)
         rospy.Subscriber("/Object_topic", ObjectStatusList, self.object_info_callback)
         self.global_path_pub = rospy.Publisher('/global_path',Path, queue_size=1)
+        self.local_path_pub = rospy.Publisher('/local_path',Path, queue_size=1)
         self.lc_1=Path()
         self.lc_1.header.frame_id='/map'
         self.lc_2=Path()
@@ -49,18 +51,43 @@ class lc_path_pub :
         self.is_object_info = False
         self.is_status = False
 
+        self.local_path_size = 25
+
+        global_path = self.lc_1
+
         rate = rospy.Rate(10) # 10hz
         while not rospy.is_shutdown():
             if self.is_object_info == True and self.is_status == True:
-                global_obj,local_obj=self.calc_vaild_obj([self.status_msg.position.x,self.status_msg.position.y,(self.status_msg.heading)/180*pi])
-                global_path = self.lc_1
+
+                global_obj,local_obj = self.calc_vaild_obj([self.status_msg.position.x,self.status_msg.position.y,(self.status_msg.heading)/180*pi])
+                self.local_path_make(global_path)
+
+                self.Check_Object(self.local_path_msg,global_obj,local_obj)
+                
+                if self.object[0] == True:
+                    if global_path != self.lc_1:
+                        global_path = self.lc_1
+                    else:
+                        global_path = self.lc_2
+
+                self.local_path_pub.publish(self.local_path_msg)
                 self.global_path_pub.publish(global_path)
 
             rate.sleep()
 
     def statusCB(self,data): ## Vehicle Status Subscriber 
         self.status_msg = data
+        self.x = self.status_msg.position.x
+        self.y = self.status_msg.position.y
         self.is_status = True
+        # 브로드캐스터 생성
+        br = tf.TransformBroadcaster()
+        # Ego 상태 tf 브로드캐스팅
+        br.sendTransform((self.status_msg.position.x, self.status_msg.position.y, 2),
+                        tf.transformations.quaternion_from_euler(0,0, self.status_msg.heading/180*pi),
+                        rospy.Time.now(),
+                        "Ego",
+                        "map")
 
     def object_info_callback(self,data): ## Object information Subscriber
         self.is_object_info = True
@@ -75,7 +102,39 @@ class lc_path_pub :
             object_pose_y.append(data.npc_list[num].position.y)
             object_velocity.append(data.npc_list[num].velocity.x)
 
-        self.object_info=[object_type,object_pose_x,object_pose_y,object_velocity]
+        self.object_info=[object_type,object_pose_x,object_pose_y,object_velocity]   
+    
+    def local_path_make(self,global_path):
+                
+        self.local_path_msg=Path()
+        self.local_path_msg.header.frame_id='/map'
+        
+        x=self.x
+        y=self.y
+        min_dis=float('inf')
+        current_waypoint=-1
+        for i,waypoint in enumerate(global_path.poses) :
+            distance=sqrt(pow(x-waypoint.pose.position.x,2)+pow(y-waypoint.pose.position.y,2))
+            if distance < min_dis :
+                min_dis=distance
+                current_waypoint=i
+        
+        if current_waypoint != -1 :
+            if current_waypoint + self.local_path_size < len(global_path.poses):
+                for num in range(current_waypoint,current_waypoint + self.local_path_size ) :
+                    tmp_pose=PoseStamped()
+                    tmp_pose.pose.position.x=global_path.poses[num].pose.position.x
+                    tmp_pose.pose.position.y=global_path.poses[num].pose.position.y
+                    tmp_pose.pose.orientation.w=1
+                    self.local_path_msg.poses.append(tmp_pose)                    
+            else :
+                for num in range(current_waypoint,len(global_path.poses) ) :
+                    tmp_pose=PoseStamped()
+                    tmp_pose.pose.position.x=global_path.poses[num].pose.position.x
+                    tmp_pose.pose.position.y=global_path.poses[num].pose.position.y
+                    tmp_pose.pose.orientation.w=1
+                    self.local_path_msg.poses.append(tmp_pose)
+
 
     def calc_vaild_obj(self,ego_pose):  # x, y, heading
         global_object_info=[]
@@ -95,10 +154,23 @@ class lc_path_pub :
             local_result=tmp_det_t.dot(global_result)
             if local_result[0][0]> 0 :
                 global_object_info.append([self.object_info[0][num],self.object_info[1][num],self.object_info[2][num],self.object_info[3][num]])
-                loal_object_info.append([self.object_info[0][num],local_result[0][0],local_result[1][0],self.object_info[3][num]])
-        
+                loal_object_info.append([self.object_info[0][num],local_result[0][0],local_result[1][0],self.object_info[3][num]])        
 
         return global_object_info,loal_object_info
+
+    def Check_Object(self,ref_path,global_vaild_object,local_vaild_object): ## 경로상의 장애물 유무 확인 (차량, 사람, 정지선 신호) ##
+        self.object=[False,0]
+        if len(global_vaild_object) >0  :
+            min_rel_distance=float('inf')
+            for i in range(len(global_vaild_object)):
+                for path in ref_path.poses :   
+                    if global_vaild_object[i][0]==1 or global_vaild_object[i][0]==2 :  
+                        dis=sqrt(pow(path.pose.position.x-global_vaild_object[i][1],2)+pow(path.pose.position.y-global_vaild_object[i][2],2))
+                        if dis<2.5:
+                            rel_distance= sqrt(pow(local_vaild_object[i][1],2)+pow(local_vaild_object[i][2],2))                            
+                            if rel_distance < min_rel_distance:
+                                min_rel_distance=rel_distance
+                                self.object=[True,i]
 
 if __name__ == '__main__':
     try:
