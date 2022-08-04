9dd8e9051c88c52b8d92ff322c4cf30dc71b22de
mdmdzero <mdmdzerozero@naver.com>
2022-08-04 15:32:25 +0900

    수정 사항 반영

--- ssafy_3/scripts/advanced_purepursuit.py
+++ ssafy_3/scripts/advanced_purepursuit.py
@@ -34,9 +34,9 @@ class pure_pursuit :
 
         #TODO: (1) subscriber, publisher 선언
         rospy.Subscriber("/global_path", Path, self.global_path_callback) 
+        rospy.Subscriber("/local_path", Path, self.path_callback)
         # rospy.Subscriber("/lane_path", Path, self.path_callback)
         # rospy.Subscriber("/lattice_path", Path, self.path_callback)
-        rospy.Subscriber("/local_path", Path, self.path_callback)
         rospy.Subscriber("/odom", Odometry, self.odom_callback)
         rospy.Subscriber("/Ego_topic",EgoVehicleStatus, self.status_callback) 
         self.ctrl_cmd_pub = rospy.Publisher('ctrl_cmd',CtrlCmd, queue_size=1)
